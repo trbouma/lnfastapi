d7c992835626b7b1c67a4fc21cbe8d613e3bba04
trbouma <trbouma@gmail.com>
2022-06-27 21:19:18 -0400

    added nav

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/resources", StaticFiles(directory="resources"), name="resources")
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
@@ -21,9 +24,16 @@ async def page(request: Request, page_name: str):
     }
     return templates.TemplateResponse("page.html", {"request": request, "data": data})
 
-@app.get("/user/{user_name}", response_class=HTMLResponse)
+@app.get("/user/v1/{user_name}", response_class=HTMLResponse)
 async def user_link(request: Request, user_name: str):
     data = {
-        "page": user_name
+        "page": user_name +"v1"
+    }
+    return templates.TemplateResponse("user.html", {"request": request, "data": data})
+
+@app.get("/user/v2/{user_name}", response_class=HTMLResponse)
+async def user_link_v2(request: Request, user_name: str):
+    data = {
+        "page": user_name +"v2"
     }
     return templates.TemplateResponse("user.html", {"request": request, "data": data})
